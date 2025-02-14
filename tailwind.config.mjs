e1e1b95518a73dda6a6c65b67688d145822d2750
Laegas <krystof.spiller@gmail.com>
2025-02-14 15:23:46 +0100

    prettier + footer client side date

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,24 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}'],
-	theme: {
-		extend: {
-			backgroundImage: {
-				'gradient-accent': 'linear-gradient(45deg, #e6c75f, #e65f5f)',
-			},
-			colors: {
-				'orange':'#e6805f'
-			},
-			keyframes: {
-				dash: {
-					'0%': { 'stroke-dashoffset': 820 },
-					'100%': { 'stroke-dashoffset': 0 },
-				},
-			},
-			animation: {
-				dash: 'dash 2s ease-in-out'
-			}
-		},
-	},
-	plugins: [],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-accent": "linear-gradient(45deg, #e6c75f, #e65f5f)",
+      },
+      colors: {
+        orange: "#e6805f",
+      },
+      keyframes: {
+        dash: {
+          "0%": { "stroke-dashoffset": 820 },
+          "100%": { "stroke-dashoffset": 0 },
+        },
+      },
+      animation: {
+        dash: "dash 2s ease-in-out",
+      },
+    },
+  },
+  plugins: [],
 }
