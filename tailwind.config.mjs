d59e649e25ec226f49a5ea98ea54dc8270bc2658
Laegas <krystof.spiller@gmail.com>
2025-02-14 14:07:55 +0100

    add tooltips and gradient; cleanup stylin

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,9 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}'],
 	theme: {
 		extend: {
+			backgroundImage: {
+				'gradient-accent': 'linear-gradient(45deg, #e6c75f, #e65f5f)',
+			},
 			colors: {
 				'orange':'#e6805f'
 			},
