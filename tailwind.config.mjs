caa651f69bd1a231c41eba8f024578e989c3ab60
Laegas <krystof.spiller@gmail.com>
2025-02-16 01:35:35 +0100

    fix link transition, Prettier, fun at parties gradient on hover

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,7 +5,8 @@ export default {
     extend: {
       backgroundImage: {
         "gradient-accent": "linear-gradient(45deg, #e6c75f, #e65f5f)",
-        "light-glow-accent": "radial-gradient(#e6815f30, transparent 70%, transparent)"
+        "light-glow-accent":
+          "radial-gradient(#e6815f30, transparent 70%, transparent)",
       },
       colors: {
         orange: "#e6805f",
@@ -16,18 +17,18 @@ export default {
           "100%": { "stroke-dashoffset": 0 },
         },
         wave: {
-          "0%": { 'transform': 'rotate(0)' },
-          "12.5%": { 'transform': 'rotate(12deg)' },
-          "37.5%": { 'transform': 'rotate(-12deg)' },
-          "62.5%": { 'transform': 'rotate(12deg)' },
-          "87.5%": { 'transform': 'rotate(-12deg)' },
-          "100%": { 'transform': 'rotate(0)' },
+          "0%": { transform: "rotate(0)" },
+          "12.5%": { transform: "rotate(12deg)" },
+          "37.5%": { transform: "rotate(-12deg)" },
+          "62.5%": { transform: "rotate(12deg)" },
+          "87.5%": { transform: "rotate(-12deg)" },
+          "100%": { transform: "rotate(0)" },
         },
       },
       animation: {
         dash: "dash 2s ease-in-out",
         wave: "wave 2s linear",
-        'wave-infinite': "wave 2s linear infinite",
+        "wave-infinite": "wave 2s linear infinite",
       },
     },
   },
