71e9d58304077724236537e40811ca421ae29ba8
krystofspiller <krystof.spiller@gmail.com>
2025-08-17 14:42:33 +0200

    update with Portchain

--- src/components/Link.test.ts
+++ src/components/Link.test.ts
@@ -1,52 +1,54 @@
 import { experimental_AstroContainer as AstroContainer } from "astro/container"
-import { expect, test } from "vitest"
+import { describe, expect, test } from "vitest"
 import Link from "./Link.astro"
 
-test.each([
-  ["https://", "https://example.com"],
-  ["mailto:", "mailto:email@example.com"],
-])("has target _blank if href starts with %s", async (_, href) => {
-  const container = await AstroContainer.create()
-  const result = await container.renderToString(Link, { props: { href } })
-
-  expect(result).toContain('target="_blank"')
-})
-
-test.each([
-  ["https://", "https://example.com"],
-  ["mailto:", "mailto:email@example.com"],
-])(
-  "target can be overwritten to _self if href starts with %s",
-  async (_, href) => {
+describe("Link", () => {
+  test.each([
+    ["https://", "https://example.com"],
+    ["mailto:", "mailto:email@example.com"],
+  ])("has target _blank if href starts with %s", async (_, href) => {
     const container = await AstroContainer.create()
-    const result = await container.renderToString(Link, {
-      props: { href, target: "_self" },
-    })
-
-    expect(result).toContain('target="_self"')
-  },
-)
-
-test.each([["# (anchor)", "#anchor"]])(
-  "has target _self if href starts with %s",
-  async (_, href) => {
-    const container = await AstroContainer.create()
-    const result = await container.renderToString(Link, {
-      props: { href },
-    })
-
-    expect(result).toContain('target="_self"')
-  },
-)
-
-test.each([["# (anchor)", "#anchor"]])(
-  "target can be overwritten to _blank if href starts with %s",
-  async (_, href) => {
-    const container = await AstroContainer.create()
-    const result = await container.renderToString(Link, {
-      props: { href, target: "_blank" },
-    })
+    const result = await container.renderToString(Link, { props: { href } })
 
     expect(result).toContain('target="_blank"')
-  },
-)
+  })
+
+  test.each([
+    ["https://", "https://example.com"],
+    ["mailto:", "mailto:email@example.com"],
+  ])(
+    "target can be overwritten to _self if href starts with %s",
+    async (_, href) => {
+      const container = await AstroContainer.create()
+      const result = await container.renderToString(Link, {
+        props: { href, target: "_self" },
+      })
+
+      expect(result).toContain('target="_self"')
+    },
+  )
+
+  test.each([["# (anchor)", "#anchor"]])(
+    "has target _self if href starts with %s",
+    async (_, href) => {
+      const container = await AstroContainer.create()
+      const result = await container.renderToString(Link, {
+        props: { href },
+      })
+
+      expect(result).toContain('target="_self"')
+    },
+  )
+
+  test.each([["# (anchor)", "#anchor"]])(
+    "target can be overwritten to _blank if href starts with %s",
+    async (_, href) => {
+      const container = await AstroContainer.create()
+      const result = await container.renderToString(Link, {
+        props: { href, target: "_blank" },
+      })
+
+      expect(result).toContain('target="_blank"')
+    },
+  )
+})
---
+++ src/components/YearRange.test.ts
@@ -0,0 +1,68 @@
+import { experimental_AstroContainer as AstroContainer } from "astro/container"
+import { describe, expect, test } from "vitest"
+import YearRange from "./YearRange.astro"
+import { JSDOM } from "jsdom"
+
+const currentYear = new Date().getFullYear()
+
+async function renderYearRangeGetTextContent(
+  from: number,
+  to?: number,
+  note?: string,
+) {
+  const container = await AstroContainer.create()
+  const dom = new JSDOM(
+    await container.renderToString(YearRange, {
+      props: { from, to, note },
+    }),
+  )
+
+  return dom.window.document.querySelector("span")?.textContent
+}
+
+describe("YearRange", () => {
+  test("return range given valid from and to", async () => {
+    let result = await renderYearRangeGetTextContent(2022, 2024)
+
+    expect(result).toContain("2022-2024")
+
+    result = await renderYearRangeGetTextContent(2022, currentYear)
+
+    expect(result).toContain(`2022-${currentYear}`)
+  })
+
+  test("return range with note given valid from and to and note", async () => {
+    const note = "part-time"
+    let result = await renderYearRangeGetTextContent(2022, 2024, note)
+
+    expect(result).toContain(`2022-2024 (${note})`)
+
+    result = await renderYearRangeGetTextContent(2022, currentYear, note)
+
+    expect(result).toContain(`2022-${currentYear} (${note})`)
+  })
+
+  test("return from to Now given larger from than to if from is current year", async () => {
+    const result = await renderYearRangeGetTextContent(currentYear, 2022)
+
+    expect(result).toBe(`${currentYear}-Now`)
+  })
+
+  test("return from to Now given from as current year", async () => {
+    const result = await renderYearRangeGetTextContent(currentYear)
+
+    expect(result).toBe(`${currentYear}-Now`)
+  })
+
+  test("return range to current year given from smaller than current year", async () => {
+    const result = await renderYearRangeGetTextContent(2023)
+
+    expect(result).toBe(`2023-${currentYear}`)
+  })
+
+  test("return range to current year given larger from than to and from being smaller than current year", async () => {
+    const result = await renderYearRangeGetTextContent(2024, 2022)
+
+    expect(result).toBe(`2024-${currentYear}`)
+  })
+})
