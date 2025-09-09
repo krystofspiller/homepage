30c1b7f0135ee4fc84612195240041b93e06392a
krystofspiller <krystof.spiller@gmail.com>
2025-09-10 01:17:27 +0200

    remove debugging

--- src/data/common.ts
+++ src/data/common.ts
@@ -3,6 +3,5 @@ import { getCollection } from "astro:content"
 import { env } from "../../env"
 
 export const posts = await getCollection("blog", ({ data }) => {
-  console.log("env.PROD", env.PROD)
   return env.PROD ? !data.draft : true
 })
