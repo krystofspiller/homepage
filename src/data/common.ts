5db51491154847832219a751312fc57db2ed6dda
krystofspiller <krystof.spiller@gmail.com>
2025-09-10 01:10:34 +0200

    debug prod variable on dynamic route with getStaticPaths

--- src/data/common.ts
+++ src/data/common.ts
@@ -3,5 +3,6 @@ import { getCollection } from "astro:content"
 import { env } from "../../env"
 
 export const posts = await getCollection("blog", ({ data }) => {
+  console.log("env.PROD", env.PROD)
   return env.PROD ? !data.draft : true
 })
