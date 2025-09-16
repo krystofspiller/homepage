be5b4f555f316eae35d8d9e47b881ef41c474fa2
krystofspiller <krystof.spiller@gmail.com>
2025-09-16 23:17:24 +0200

    add blog versions

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,13 +1,49 @@
+import fs from "fs"
+
+import { getBlogPostVersionContent, getBlogPostVersions } from "@utils/github"
 import { glob } from "astro/loaders"
 import { defineCollection, z } from "astro:content"
 
-const blog = defineCollection({
-  loader: glob({ pattern: "**/*.mdx", base: "./src/data/blog" }),
-  schema: z.object({
-    title: z.string(),
-    pubDate: z.date(),
-    draft: z.boolean().optional(),
-  }),
+export const commonSchema = z.object({
+  title: z.string(),
+  pubDate: z.coerce.date(),
+  draft: z.boolean().optional(),
 })
 
-export const collections = { blog }
+const blog = defineCollection<typeof commonSchema>({
+  loader: glob({ pattern: "**/*.mdx", base: "./src/data/blog" }),
+  schema: commonSchema,
+})
+
+const fileNames = fs.readdirSync("./src/data/blog")
+
+for (const fileName of fileNames) {
+  const fileNameWithoutExtension = fileName.replace(".mdx", "")
+  const versions = await getBlogPostVersions(fileNameWithoutExtension)
+
+  if (!fs.existsSync(`./src/data/blog-versions`)) {
+    fs.mkdirSync(`./src/data/blog-versions`)
+  }
+
+  await Promise.all(
+    versions.map(async (version) => {
+      const content = await getBlogPostVersionContent(
+        fileNameWithoutExtension,
+        version.sha,
+      )
+      if (content) {
+        fs.writeFileSync(
+          `./src/data/blog-versions/${fileNameWithoutExtension}-${version.sha}.mdx`,
+          content,
+        )
+      }
+    }),
+  )
+}
+
+const blogVersions = defineCollection<typeof commonSchema>({
+  loader: glob({ pattern: "**/*.mdx", base: "./src/data/blog-versions" }),
+  schema: commonSchema,
+})
+
+export const collections = { blog, blogVersions }
--- src/data/common.ts
+++ src/data/common.ts
@@ -5,3 +5,7 @@ import { env } from "../../env"
 export const posts = await getCollection("blog", ({ data }) => {
   return env.PROD ? !data.draft : true
 })
+
+export const postsVersions = await getCollection("blogVersions", ({ data }) => {
+  return env.PROD ? !data.draft : true
+})
