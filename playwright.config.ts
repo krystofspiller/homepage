1080f04936f6b9c95fb340316975d41345720844
krystofspiller <krystof.spiller@gmail.com>
2025-09-20 12:22:31 +0200

    remove console logs

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,7 +4,6 @@ import { defineConfig, devices } from "@playwright/test"
 import { env } from "./env"
 
 const isCi = env.ENV === "ci"
-console.log("DEBUG isCi", isCi)
 
 /**
  * See https://playwright.dev/docs/test-configuration.
