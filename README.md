24321b77913a9eff83df7b971215ae3f76daae01
krystofspiller <krystof.spiller@gmail.com>
2025-10-27 22:48:04 +0100

    feat: extract API functions into separate files, add tests; update todos; add vitest; use nav and main HTML tags

--- README.md
+++ README.md
@@ -4,6 +4,4 @@ Welcome to the source code of my homepage built with Astro!
 
 <!-- TODO: explore view transitions further, make animation of headings from blog index to article headings -->
 <!-- TODO: can we recreate something like the scrolling snap in gsap? basically keep some of the momentum of the animation, overshoot and snap back -->
-<!-- TODO: Refactor tooltips to address accessibility issues (Floating UI?) -->
 <!-- TODO: Setup StoryBook - split components into design system and components -->
-<!-- TODO: test getLatestYouTubeVideo -->
---
+++ src/utils/youtube.ts
@@ -0,0 +1,38 @@
+import { YOUTUBE_VIDEO_ID_DEFAULT } from "./constants"
+import { env } from "env"
+import { z } from "zod"
+
+const youtubeVideosSchema = z.object({
+  items: z.array(
+    z.object({
+      contentDetails: z.object({
+        videoId: z.string(),
+        videoPublishedAt: z.string(),
+      }),
+    }),
+  ),
+})
+
+export const getLatestYouTubeVideo = async (): Promise<string> => {
+  try {
+    const youtubeVideosUrl = `https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults=50&playlistId=PL7-fKc8SwbfOk3eZyIk_sbKjR7YMO6lfW&key=${env.YOUTUBE_API_KEY}`
+    const response = await fetch(youtubeVideosUrl)
+    if (!response.ok) {
+      return YOUTUBE_VIDEO_ID_DEFAULT
+    }
+
+    const json = await response.json()
+    const { items } = youtubeVideosSchema.parse(json)
+    let acc: [string, number] = ["id", 0]
+    for (const item of items) {
+      const date = Date.parse(item.contentDetails.videoPublishedAt)
+      if (date > acc[1]) {
+        acc = [item.contentDetails.videoId, date]
+      }
+    }
+
+    return acc[0]
+  } catch {
+    return YOUTUBE_VIDEO_ID_DEFAULT
+  }
+}
