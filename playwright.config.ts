6c7d2622d14888e4d070b0120ea06bfff07266f9
krystofspiller <krystof.spiller@gmail.com>
2025-11-16 09:48:25 +0100

    fix: use pnpm in playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -36,7 +36,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "npm run dev",
+    command: "pnpm run dev",
     reuseExistingServer: !isCi,
     url: "http://localhost:4321",
   },
